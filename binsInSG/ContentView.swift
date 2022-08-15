1a8a28aa7ebd10fcd96b04087a2a189204cf69b2
thamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2022-08-15 11:53:20 +0800

    Initial Commit

---
+++ binsInSG/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  binsInSG
+//
+//  Created by Tham En Jie on 15/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ binsInSG/binsInSGApp.swift
@@ -0,0 +1,17 @@
+//
+//  binsInSGApp.swift
+//  binsInSG
+//
+//  Created by Tham En Jie on 15/8/22.
+//
+
+import SwiftUI
+
+@main
+struct binsInSGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
